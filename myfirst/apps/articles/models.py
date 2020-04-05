1e9324625d1884b04636a2610ef37a5e0496c4d3
Ihor <ihor917@gmail.com>
2020-04-05 23:22:20 +0300

    Project backup. First commit

---
+++ myfirst/apps/articles/models.py
@@ -0,0 +1,23 @@
+import datetime
+from django.db import models
+
+from django.utils import timezone
+
+class Article(models.Model):
+    article_title = models.CharField('title', max_length=200)
+    article_text = models.TextField('content')
+    pub_date = models.DateTimeField('publication date')
+
+    def __str__(self):
+        return self.article_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days=7))
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author_name = models.CharField('comment author', max_length=50)
+    comment_text = models.CharField('comment text', max_length=200)
+
+    def __str__(self):
+        return self.author_name
---
+++ myfirst/apps/articles/views.py
@@ -0,0 +1,31 @@
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+
+from django.urls import reverse
+
+from .models import Article
+
+def index(request):
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'articles/list.html', {'latest_articles': latest_articles_list})
+
+def detail(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404('Articles not found!')
+
+    latest_comments_list = a.comment_set.order_by('-id')[:10]
+
+    return render(request, 'articles/detail.html', {'article': a,
+                                                    'latest_comments': latest_comments_list})
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404('Articles not found!')
+
+    a.comment_set.create(author_name=request.POST['name'], comment_text=request.POST['text'])
+
+    return HttpResponseRedirect( reverse('articles:detail', args=(a.id,)) )
